a6a780efc656bb0ddd372894eba4e13956765dff
Bekzodjon Norkuziev <bekzodnx@gmail.com>
2021-06-27 00:46:47 +0200

    Added the new Picker

--- src/api.js
+++ src/api.js
@@ -6,15 +6,16 @@ export function getExchangeRates(base, supportedCurrencies) {
     .filter((currency) => currency !== base)
     .join();
 
-  const url = `http://api.exchangeratesapi.io/latest?base=${base}&symbols=${symbols}`;
-  fetch(url)
+  const url = `http://api.exchangeratesapi.io/latest?base=${base}&symbols=${currencies}`;
+  return fetch(url)
     .then((res) => res.json())
-    .then(handleAPIErrors);
+    .then(handleAPIErrors)
+    .then((res) => res.rates);
 }
 
 function handleAPIErrors(res) {
   if (res.success) return res;
-  console.error(`Server Error:`);
+  console.error('Server Error');
   return {
     rates: {
       USD: 1.0,
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,36 @@
 import { useCallback, useState, useEffect } from 'react';
+import { getExchangeRates } from '../api';
+import { CurrencyCodePicker } from './CurrencyCodePicker';
 
-const supportedCurrencies = ['USD', 'EUR', 'JPY', 'CAD', 'GBP', 'MXN'];
+const supportedCurrencies = ['USD', 'EUR', 'JPY', 'CAD', 'GBP', 'HUF'];
 export function Main() {
   const [amount, setAmount] = useState('1.50');
   const [currencyCode, setCurrentCode] = useState('USD');
   const [currencyData, setCurrencyData] = useState({ USD: 1.0 });
 
+  useEffect(() => {
+    getExchangeRates(currencyCode, supportedCurrencies).then((rates) => {
+      setCurrencyData(rates);
+    });
+  }, [currencyCode]);
+
+  const handleCurrencyChange = (e) => {
+    setCurrentCode(e.target.value);
+  };
+
+  const handleAmountChange = (e) => {
+    setAmount(e.target.value);
+  };
+
   return (
     <>
       <section>
         <h1>Exchange Rates</h1>
+        <CurrencyCodePicker
+          supportedCurrencies={supportedCurrencies}
+          currencyCode={currencyCode}
+          onChange={handleCurrencyChange}
+        />
       </section>
     </>
   );
